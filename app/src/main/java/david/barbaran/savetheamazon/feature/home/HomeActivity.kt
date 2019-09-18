af0e1b01272cd5233f8ac913cce4d9fe7ee64afc
David Barbaran <alejandro.david.barbaran@gmail.com>
2019-09-18 13:59:05 -0500

    Add koin and set viewmodel

--- app/src/main/java/david/barbaran/savetheamazon/feature/home/HomeActivity.kt
+++ app/src/main/java/david/barbaran/savetheamazon/feature/home/HomeActivity.kt
@@ -6,43 +6,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import david.barbaran.savetheamazon.R
-import david.barbaran.savetheamazon.feature.home.presenter.HomePresenter
 import david.barbaran.savetheamazon.feature.home.presenter.HomeView
+import david.barbaran.savetheamazon.feature.home.viewmodel.HomeViewModel
 import david.barbaran.savetheamazon.model.Donate
 import david.barbaran.savetheamazon.util.RevealAnimation
 import kotlinx.android.synthetic.main.activity_home.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeActivity : AppCompatActivity(), HomeView {
 
     private var mRevealAnimation: RevealAnimation? = null
-    //private var homeViewModel: HomeViewModel? = null
+    private val homeViewModel: HomeViewModel by viewModel()
     private val donateAdapter = DonateAdapter()
-
-    private var homePresenter : HomePresenter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        homePresenter = HomePresenter(this)
-        homePresenter?.homeView = this
         setRecycler()
+        setViewModel()
         setWindow()
         initReveal()
     }
 
     /** Init methods **/
 
-    /*
     private fun setViewModel() {
-        homeViewModel = ViewModelProviders.of(this)[HomeViewModel::class.java]
-        homeViewModel?.apply {
+        homeViewModel.apply {
             donateLiveData.observe(this@HomeActivity, getDonateObserver())
         }
     }
-     */
 
     private fun setWindow() {
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -60,8 +54,7 @@ class HomeActivity : AppCompatActivity(), HomeView {
         mRevealAnimation?.onFinishReveal = object : RevealAnimation.OnFinishReveal {
             override fun onFinishReveal() {
                 setStatusBar()
-                homePresenter?.getDonate()
-                // homeViewModel?.getDonate()
+                homeViewModel.getDonate()
             }
         }
     }
@@ -91,6 +84,6 @@ class HomeActivity : AppCompatActivity(), HomeView {
         shimmerLayout.hideShimmer()
         shimmerLayout.visibility = View.GONE
         donateAdapter.list = it
-        donateAdapter.notifyItemRangeInserted(0, it.size)
+        donateAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/david/barbaran/savetheamazon/feature/home/di/HomeModule.kt
@@ -0,0 +1,10 @@
+package david.barbaran.savetheamazon.feature.home.di
+
+import david.barbaran.savetheamazon.feature.home.viewmodel.HomeViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val homeModule = module {
+    viewModel { HomeViewModel(androidApplication()) }
+}
