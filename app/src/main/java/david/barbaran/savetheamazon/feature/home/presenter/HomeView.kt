f1b4927181547a973d1d2ac31c69a99f56ccbff2
David Barbaran <alejandro.david.barbaran@gmail.com>
2019-09-24 09:47:57 -0500

    Enable incremental apt and remove presenter example

--- app/src/main/java/david/barbaran/savetheamazon/feature/home/presenter/HomeView.kt
+++
@@ -1,7 +0,0 @@
-package david.barbaran.savetheamazon.feature.home.presenter
-
-import david.barbaran.savetheamazon.model.Donate
-
-interface HomeView {
-    fun shoDonates(list : MutableList<Donate>)
-}
